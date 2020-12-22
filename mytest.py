ecb2e1699870777915fa84c8c88c8a831b204770
jinzheng1050 <zheng.jin1050@gmail.com>
2020-12-22 13:22:28 -0800

    added file open code

--- mytest.py
+++ mytest.py
@@ -1,2 +1,16 @@
-print('Hello from Python')
+def read_input_file():
+    with open('text', 'r') as f:
+        line = f.readline()
+        while line:
+            print(line)
+            line = f.readdline()
+
+def write_output_file():
+    print('To be done')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('Start my python')
+    read_input_file()
+    write_output_file()
 
