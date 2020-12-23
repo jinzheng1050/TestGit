a95096cebf04477806f2fa2e651d941da69597a8
jinzheng1050 <zheng.jin1050@gmail.com>
2020-12-22 22:14:15 -0800

    test assert

--- test_mytest.py
+++ test_mytest.py
@@ -3,7 +3,7 @@ import mytest
 class TestMytest:
 
     def test_read_input_file(self):
-        assert 5 == mytest.read_input_file('log.txt')
+        assert 4 == mytest.read_input_file('log.txt')
 
     def test_write_output_file(self):
-        assert 293 == mytest.write_output_file()
+        assert 294 == mytest.write_output_file()
