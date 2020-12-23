ed8144fa55cc5ce6f4f1b4be77b9b43759624e86
jinzheng1050 <zheng.jin1050@gmail.com>
2020-12-22 21:59:01 -0800

    fixed type

--- test_mytest.py
+++ test_mytest.py
@@ -3,9 +3,7 @@ import mytest
 class TestMytest:
 
     def test_read_input_file(self):
-#        asset 5 == mytest.read_input_file()
-        pass
+        assert 5 == mytest.read_input_file()
 
     def test_write_output_file(self):
-#        asset 294 == mytest.write_output_file()
-        pass
+        assert 294 == mytest.write_output_file()
