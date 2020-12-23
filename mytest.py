d03a53c2c6daf98893f6587baff9ff720701046e
jinzheng1050 <zheng.jin1050@gmail.com>
2020-12-22 21:12:29 -0800

    added returned values for testing

--- mytest.py
+++ mytest.py
@@ -1,31 +1,36 @@
 import json
 
 def read_input_file():
+    line_read = 0
     with open('text', 'r') as f:
         line = f.readline()
+        line_read += 1
         while line:
             print(line)
             line = f.readline()
+            lin_read += 1
+    return line_read
 
 def write_output_file():
-    with open('out.json', 'w') as f:
-        dataout = {
-            "total_number_of_lines_processed": 4,
-            "total_number_of_lines_ok": 4,
-            "total_number_of_lines_failed": 0,
-            "top_client_ips": {
-                "92.177.30.4": 1,
-                "51.232.15.21": 1,
-                "34.149.47.34": 1,
-                "112.21.100.55": 1
-            },
-            "top_path_avg_seconds": {
-                "/admin.php": 0.05,
-                "/product/catalog": 1.09,
-                "/product/cart": 1.2
-            }
+    dataout = {
+        "total_number_of_lines_processed": 4,
+        "total_number_of_lines_ok": 4,
+        "total_number_of_lines_failed": 0,
+        "top_client_ips": {
+            "92.177.30.4": 1,
+            "51.232.15.21": 1,
+            "34.149.47.34": 1,
+            "112.21.100.55": 1
+        },
+        "top_path_avg_seconds": {
+            "/admin.php": 0.05,
+            "/product/catalog": 1.09,
+            "/product/cart": 1.2
         }
+    }
+    with open('out.json', 'w') as f:
         json.dump(dataout, f)
+    return len(json.dumps(dataout))
 
 def process_data():
     print('Log parsing and processing.....')
