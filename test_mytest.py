80eab69eb90151181417aaa06a322076365994dd
jinzheng1050 <zheng.jin1050@gmail.com>
2020-12-23 20:18:42 -0800

    added class test

--- test_mytest.py
+++ test_mytest.py
@@ -11,9 +11,9 @@ class TestMytest:
         assert 294 == mytest.write_output_file()
 
     def test_plus_ab(self):
-        lp = LogParser()
-        assert 7 == lp.plus_ab(3,4)
+        lp = LogParser(3,4)
+        assert 7 == lp.plus_ab()
 
     def test_multiply_ab(self):
-        jp = LogParser()
-        assert 18 == lp.multiply(3,6)
+        lp = LogParser(3,6)
+        assert 18 == lp.multiply()
