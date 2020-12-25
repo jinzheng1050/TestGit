e78f393eb35f018f4967cf6745561e21275359de
jinzheng1050 <zheng.jin1050@gmail.com>
2020-12-25 10:58:25 -0800

    added cmd flag process

--- mytest.py
+++ mytest.py
@@ -1,9 +1,23 @@
 import json, sys, re
 from log_parser import LogParser
+import configparser,traceback,inspect
 
 def load_config():
-    def_settings = {'in':'log.txt', 'out':'results.json','max-client-ips':10, 'max-path':10}
+    def_settings = {}
+    try:
+        config = configparser.RawConfigParser()
+        config.read('log_parser.conf')
+        def_settings['in']              = config.get('LOG_PARSER', 'INPUT_FILE')
+        def_settings['out']             = config.get('LOG_PARSER', 'OUTPUT_FILE')
+        def_settings['max-client-ips']  = config.get('LOG_PARSER', 'MAX_CLIENT_IPS')
+        def_settings['max-path']        = config.get('LOG_PARSER', 'MAX_PATH')
+
+    except NoSectionError, NoOptionError:
+
+        def_settings = {'in':'log.txt', 'out':'results.json','max-client-ips':10, 'max-path':10}
+
     return def_settings
+
 
 def get_settings(cmd):
     print(cmd)
@@ -18,7 +32,7 @@ def get_settings(cmd):
             return 'Invalid'
         settings[flag[1:]] = cmd[i+1]
         if flag[1:] in ('max-client-ips', 'max-path'):
-            if not cmd[i+1].isnumeric():
+            if not cmd[i+1].isdight():
                 return None
             value = int(cmd[i+1])
             if value < 1 or value > 10000:
