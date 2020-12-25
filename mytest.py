6decc580bbbc674ba5e982f294bb6cafa9ac3923
jinzheng1050 <zheng.jin1050@gmail.com>
2020-12-25 11:49:24 -0800

    bug fix

--- mytest.py
+++ mytest.py
@@ -13,8 +13,8 @@ def load_config():
         def_settings['max-client-ips']  = config.get('LOG_PARSER', 'MAX_CLIENT_IPS')
         def_settings['max-path']        = config.get('LOG_PARSER', 'MAX_PATH')
 
-#    except (NoSectionError, NoOptionError):
-    except:
+    except (NoSectionError, NoOptionError):
+#    except:
         print('Incorrect config file. Use default.')
         def_settings = {'in':'log.txt', 'out':'results.json','max-client-ips':10, 'max-path':10}
 #        pass
